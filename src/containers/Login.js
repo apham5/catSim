5b122774d61249f3e9969a480431ac682723ce39
apham5 <44855661+apham5@users.noreply.github.com>
2018-12-05 02:24:36 -0500

    Update Login.js

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -31,7 +31,7 @@ export default class Login extends Component {
     event.preventDefault();
     
     try {
-      fetch('https://catsimserver.herokuapp.com/checkuser', {
+      fetch('https://catsimulator.herokuapp.com/checkuser', {
          method: 'POST',
          headers: {
               'Content-Type': 'application/json'
