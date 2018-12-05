e2b453d37445d9d273dd6402d7969b2e32dfc829
apham5 <44855661+apham5@users.noreply.github.com>
2018-12-05 02:25:46 -0500

    Update Signup.js

--- src/containers/Signup.js
+++ src/containers/Signup.js
@@ -61,7 +61,7 @@ export default class Signup extends Component {
     }
 
     try {
-      fetch('https://catsimserver.herokuapp.com/newuser', {
+      fetch('https://catsimulator.herokuapp.com/newuser', {
          method: 'POST',
          headers: {
               'Content-Type': 'application/json'
